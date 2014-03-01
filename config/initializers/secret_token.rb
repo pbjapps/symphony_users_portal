a13fcf9e157c65b2b0880fe9cccdf7416942ff34
Janoo Fernandes <smoothclip@gmail.com>
2014-03-01 21:07:50 +0000

    Initial commit of working welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SymphonyUsersPortal::Application.config.secret_key_base = '4c3c1d486fbe4f888e2ec4455b7a23203065cd366081ecbb0fd8012385b837ada6f785a9a3a0f24e474cf501ca5007ba29646a78e0a1f5c13fa9cf01408d218a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SymphonyUsersPortal::Application.config.session_store :cookie_store, key: '_symphony_users_portal_session'
